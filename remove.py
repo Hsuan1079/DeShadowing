50442e57aefec3dee10513cb41f9eafaa41268c5
Hsuan1079 <grace534266.cs13@nycu.edu.tw>
2024-12-10 17:56:36 +0800

    1210

--- remove.py
+++ remove.py
@@ -1,11 +1,11 @@
 import os
 import cv2
 
-os.makedirs("Detect", exist_ok=True)
-os.makedirs("Remove", exist_ok=True)
+os.makedirs("output/Remove", exist_ok=True)
+os.makedirs("output/new", exist_ok=True)
 os.makedirs("Data_choose", exist_ok=True)
-all_files = os.listdir("data/SRD/shadow_free")
-remove = os.listdir("Remove")
+all_files = os.listdir("output/Paired")
+remove = os.listdir("output/Remove")
 remove_t =[i.split(".")[0] for i in remove]
 remove_t = remove_t[1:]
 print(remove_t)
@@ -13,5 +13,7 @@ print(remove_t)
 # 檢查all_files裡面的檔案是否在remove裡面，如果有就移動到Data_choose資料夾
 for i in all_files:
     word = i.split(".")
-    if word[0][:-5] in remove_t:
-        cv2.imwrite("data/Data_choose_shadow_free/" + i, cv2.imread("data/SRD/shadow_free/" + i))
+    if word[0][7:] in remove_t:
+        # 把資料寫進new資料夾
+        img = cv2.imread("output/Paired/"+i)
+        cv2.imwrite("output/new/"+i, img)
